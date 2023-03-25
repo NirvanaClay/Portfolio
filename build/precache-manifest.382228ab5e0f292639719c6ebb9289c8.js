02ec252dba794f61c04a2780ba08be41c1362045
Clay <thejourney318@gmail.com>
2023-03-24 20:43:27 -0400

    Added build folder.

---
+++ build/precache-manifest.382228ab5e0f292639719c6ebb9289c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21636593d6b59a876a5fb590ff62e17e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e8492b6ad35aa56767",
+    "url": "/static/css/2.4d63e703.chunk.css"
+  },
+  {
+    "revision": "be013f9029265cc3e738",
+    "url": "/static/css/main.cac2d175.chunk.css"
+  },
+  {
+    "revision": "23e8492b6ad35aa56767",
+    "url": "/static/js/2.70dabed7.chunk.js"
+  },
+  {
+    "revision": "0b31fb14eee500646560caa49543f84c",
+    "url": "/static/js/2.70dabed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a2c612d75f6aac35f1",
+    "url": "/static/js/3.086b8de1.chunk.js"
+  },
+  {
+    "revision": "be013f9029265cc3e738",
+    "url": "/static/js/main.108ece24.chunk.js"
+  },
+  {
+    "revision": "a1ed4a4161aa0e12e002",
+    "url": "/static/js/runtime~main.b02e17ae.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
